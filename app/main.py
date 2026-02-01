8c3edab2774af10fb919c6875ce95ae42d51edad
osmonalievw <razakosmonaliev@gmail.com>
2026-02-01 19:44:05 +0900

    feat: setup postgres database connection with sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from  fastapi  import FastAPI
+from fastapi import FastAPI
+from sqlalchemy import text
+from db.session import engine
 
-app = FastAPI(
-title="UlechuBot",
-version="0.0.1"
-)
+app = FastAPI(title="UlechuBot", version="0.0.1")
+
 
 @app.get("/ping")
-async   def pong():
+async def pong():
     return {"ping": "pong", "message": "System is operationalwww"}
+
+
+@app.get("/health")
+async def health_check():
+    try:
+        async with engine.connect() as conn:
+            await conn.execute(text("SELECT 1"))
+        return {"databse": "online", "status": "healthy🟢"}
+    except Exception as e:
+        return {"databse": "offline", "error": str(e), "status": "critical 🔴"}
